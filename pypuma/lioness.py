21b72afee126e38e169909e2b458036ef3fb363e
Alessandro <AlessandroMarin80@gmail.com>
2018-08-14 14:10:37 -0400

    Tue Aug 14 14:10:37 EDT 2018

--- pypuma/lioness.py
+++ pypuma/lioness.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 import os, os.path
 import numpy as np
-from .panda import Panda
+from pypuma.puma import Puma
 from .timer import Timer
 
 class Lioness(Panda):
